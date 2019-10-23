058697f112d3bc45cf7de0ce466a92a38a451184
granttaylor448 <granttaylor448@gmail.com>
2019-10-23 16:42:41 +0000

    first push of client file

--- play.js
+++ play.js
@@ -1,33 +1,5 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.15.225',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
-  return conn;
-  
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
-
-const conn = net.createConnection({ 
-  host: '192.168.15.225',
-  port: 50541
-});
-
-conn.on('connect', () => {
-  console.log('Welcome to the party');
-});
-
-conn.on('data', () => {
-  console.log('oh no to slow!');
-});
 
