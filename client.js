4a5e154a7f3978be9fed0c7a025efbbd223616bc
granttaylor448 <granttaylor448@gmail.com>
2019-10-23 19:52:01 +0000

    pushing with two new function

--- client.js
+++ client.js
@@ -14,16 +14,22 @@ const connect = function() {
   conn.on('data', () => {
     console.log('oh no to slow!');
   });
+  // conn.on('connect', () => {
+    // console.log('Welcome to the party');
+  // });
   conn.on('connect', () => {
     console.log('Welcome to the party');
-  });
-  conn.on('connect', () => {
     conn.write('Name: GT');
+    // setTimeout(() => conn.write('Move: down'), 500);
+    // setTimeout(() => conn.write('Move: left'), 1000);
+    // setTimeout(() => conn.write('Move: up'), 1500);
+    // setTimeout(() => conn.write('Move: right'), 2000);
+    setInterval(() => conn.write('Move: up'), 500)
   });
 
-  conn.on('connect', () => {
-    conn.write('Move: down');
-  });
+  // conn.on('connect', () => {
+    // conn.write('Move: down');
+  // });
   
   return conn;
   
--- play.js
+++ play.js
@@ -3,3 +3,23 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log("Thank you for playing!")
+    process.exit();
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data',handleUserInput)
+  return stdin;
+}
+setupInput();
+
+
+
+
